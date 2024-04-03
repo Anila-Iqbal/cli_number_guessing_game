c54b1fe09808e9660d52c424aaf7bcdd8383191a
Anila <163565128+Anila-Iqbal@users.noreply.github.com>
2024-04-03 10:43:59 +0500

    second commit

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+import inquirer from "inquirer";
+// 1) computer will generate a random numbre - Done
+// 2) user input will guessing number - Done
+// 3) compare user inpit with computer -
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        message: "Please enter a number between 1-6: ",
+        type: "number"
+    },
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("Congratulation! you guessed right number.");
+}
+else {
+    console.log("You guessed wrong number.");
+}
--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
+
 import inquirer from "inquirer";
+
+// 1) computer will generate a random numbre - Done
+// 2) user input will guessing number - Done
+// 3) compare user inpit with computer - Done
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        message:"Please enter a number between 1-6: ",
+        type: "number"
+    },
+]);
+
+if (answers.userGuessedNumber === randomNumber){
+    console.log("Congratulation! you guessed right number.");
+} else {
+    console.log("You guessed wrong number.")
+}
