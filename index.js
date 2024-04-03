addc751418e7c102e267d304617b3432445e2123
Anila <163565128+Anila-Iqbal@users.noreply.github.com>
2024-04-03 12:17:47 +0500

    third commit

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
+// Printing a Welcome message:
+console.log(chalk.magentaBright.bold("\n\tWelcome to Anila-Iqbal cli_number_guessing_game\n"));
 // 1) computer will generate a random numbre - Done
 // 2) user input will guessing number - Done
-// 3) compare user inpit with computer -
+// 3) compare user inpit with computer - Done
 const randomNumber = Math.floor(Math.random() * 6 + 1);
 const answers = await inquirer.prompt([
     {
         name: "userGuessedNumber",
-        message: "Please enter a number between 1-6: ",
-        type: "number"
+        message: chalk.blueBright.bold("Please enter a number between 1-6: "),
+        type: "number",
     },
 ]);
 if (answers.userGuessedNumber === randomNumber) {
-    console.log("Congratulation! you guessed right number.");
+    console.log(chalk.greenBright.bold("\nCongratulation! you guessed right number."));
 }
 else {
-    console.log("You guessed wrong number.");
+    console.log(chalk.redBright.bold("\nYou guessed wrong number."));
 }
--- index.ts
+++ index.ts
@@ -1,5 +1,14 @@
+#! /usr/bin/env node
 
 import inquirer from "inquirer";
+import chalk from "chalk";
+
+// Printing a Welcome message:
+console.log(
+  chalk.magentaBright.bold(
+    "\n\tWelcome to Anila-Iqbal cli_number_guessing_game\n"
+  )
+);
 
 // 1) computer will generate a random numbre - Done
 // 2) user input will guessing number - Done
@@ -8,15 +17,17 @@ import inquirer from "inquirer";
 const randomNumber = Math.floor(Math.random() * 6 + 1);
 
 const answers = await inquirer.prompt([
-    {
-        name: "userGuessedNumber",
-        message:"Please enter a number between 1-6: ",
-        type: "number"
-    },
+  {
+    name: "userGuessedNumber",
+    message: chalk.blueBright.bold("Please enter a number between 1-6: "),
+    type: "number",
+  },
 ]);
 
-if (answers.userGuessedNumber === randomNumber){
-    console.log("Congratulation! you guessed right number.");
+if (answers.userGuessedNumber === randomNumber) {
+  console.log(
+    chalk.greenBright.bold("\nCongratulation! you guessed right number.")
+  );
 } else {
-    console.log("You guessed wrong number.")
+  console.log(chalk.redBright.bold("\nYou guessed wrong number."));
 }
